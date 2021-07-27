f05b293cba3f90aadd7836e084398e6a04303176
pedroiriartew <pedroiriartew@gmail.com>
2021-07-27 15:05:40 -0300

    added index and PUT,POST and DELETE methods

--- src/main/java/controllers/CreatureController.java
+++ src/main/java/controllers/CreatureController.java
@@ -1,11 +1,12 @@
 package controllers;
 
+import enums.Enviroment;
+import enums.Size;
+import enums.Type;
 import model.Creature;
+import org.springframework.web.bind.annotation.*;
 import services.CreatureService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -24,5 +25,30 @@ public class CreatureController {
     public List<Creature> getCreature()
     {
         return creatureService.getCreature();
+    }
+
+    @PostMapping
+    public void addNewCreature(@RequestBody Creature creature)
+    {
+        creatureService.addNewCreature(creature);
+    }
+
+    @DeleteMapping(path = "{creatureId}")
+    public void deleteCreature(@PathVariable("creatureId") Long id)
+    {
+        creatureService.deleteCreature(id);
+    }
+
+    @PutMapping(path = "{creatureId}")
+    public void updateCreature(@PathVariable("creatureId") Long id,
+                               @RequestParam(required = false) String name,
+                               @RequestParam(required = false)  Long challengeRating,
+                               @RequestParam(required = false) Integer experience,
+                               @RequestParam(required = false)  Size size,
+                               @RequestParam(required = false)  Type type,
+                               @RequestParam(required = false)  Enviroment enviroment,
+                               @RequestParam(required = false)  String alignment)
+    {
+        creatureService.updateCreature(id, name, challengeRating, experience, size, type, enviroment, alignment);
     }
 }
--- src/main/java/services/CreatureService.java
+++ src/main/java/services/CreatureService.java
@@ -1,11 +1,16 @@
 package services;
 
+import enums.Enviroment;
+import enums.Size;
+import enums.Type;
 import model.Creature;
 import repositories.CreatureRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CreatureService {
@@ -20,5 +25,38 @@ public class CreatureService {
     public List<Creature> getCreature()
     {
         return creatureRepository.findAll();
+    }
+
+    public void addNewCreature(Creature creature) {
+        Optional<Creature> creatureById = creatureRepository.findById(creature.getId());
+
+        if (creatureById.isPresent())
+        {
+            throw new IllegalStateException("Creature already in the database");
+        }
+
+        creatureRepository.save(creature);
+    }
+
+    public void deleteCreature(Long id) {
+        boolean exists = creatureRepository.existsById(id);
+        if (!exists)
+            throw new IllegalStateException("Creture with id: " + id +" does not exists");
+        else
+            creatureRepository.deleteById(id);
+    }
+
+    @Transactional
+    public void updateCreature(Long id, String name, Long challengeRating, Integer experience, Size size, Type type, Enviroment enviroment, String alignment) {
+        Creature creature = creatureRepository.findById(id).orElseThrow(()-> new IllegalStateException("There is no Creature with "+ id + "as ID"));
+
+        creature.setName(name);
+        creature.setChallengeRating(challengeRating);
+        creature.setExperience(experience);
+        creature.setSize(size);
+        creature.setType(type);
+        creature.setEnviroment(enviroment);
+        creature.setAlignment(alignment);
+
     }
 }
